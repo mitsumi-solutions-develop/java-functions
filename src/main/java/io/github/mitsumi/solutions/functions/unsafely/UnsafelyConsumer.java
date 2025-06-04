a144aac3da982f53a07023583c47a32d8f0219a7
mkaneyama <mkaneyama@daon.co.jp>
2025-06-05 02:19:18 +0900

    #1: javadocs: add java docs

--- src/main/java/io/github/mitsumi/solutions/functions/unsafely/UnsafelyConsumer.java
+++ src/main/java/io/github/mitsumi/solutions/functions/unsafely/UnsafelyConsumer.java
@@ -1,5 +1,12 @@
 package io.github.mitsumi.solutions.functions.unsafely;
+
 public interface UnsafelyConsumer<T> {
 
+    /**
+     * Performs this operation on the given argument.
+     *
+     * @param value the input argument
+     * @throws Exception if an error occurs while performing this operation.
+     */
     void accept(T value) throws Exception;
 }
--- src/main/java/io/github/mitsumi/solutions/functions/unsafely/UnsafelyRunner.java
+++ src/main/java/io/github/mitsumi/solutions/functions/unsafely/UnsafelyRunner.java
@@ -2,5 +2,10 @@ package io.github.mitsumi.solutions.functions.unsafely;
 
 public interface UnsafelyRunner {
 
+    /**
+     * Performs this operation.
+     *
+     * @throws Exception if an error occurs while performing this operation.
+     */
     void run() throws Exception;
 }
